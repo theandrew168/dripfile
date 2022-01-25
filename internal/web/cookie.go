0e4d4877bc8fc0e9649a8fbf32f88a293a54f767
theandrew168 <andrew@shallowbrooksoftware.com>
2022-01-25 15:45:00 -0600

    Add some basic cookie creation funcs

---
+++ internal/web/cookie.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"net/http"
+	"time"
+)
+
+var (
+	SessionIDCookieName = "session_id"
+)
+
+func GenerateSessionID() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func GenerateSessionCookie(name, value string) http.Cookie {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/", // applies to the whole site
+		Domain:   "",  // will default to the server's base domain
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	return cookie
+}
+
+func GeneratePermanentCookie(name, value string, expiry time.Time) http.Cookie {
+	// round expiration up to nearest second
+	cookie := GenerateSessionCookie(name, value)
+	cookie.Expires = time.Unix(expiry.Unix()+1, 0)
+	cookie.MaxAge = int(time.Until(expiry).Seconds() + 1)
+	return cookie
+}
+
+func GenerateExpiredCookie(name string) http.Cookie {
+	// expires now
+	cookie := GenerateSessionCookie(name, "")
+	cookie.Expires = time.Unix(1, 0)
+	cookie.MaxAge = -1
+	return cookie
+}
