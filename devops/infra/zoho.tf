d4ebd342f98cafe2b0163036d0c6432fd285aa43
theandrew168 <andrew@shallowbrooksoftware.com>
2022-03-10 12:52:45 -0600

    Add DMARC record for info@dripfile.com

--- devops/infra/zoho.tf
+++ devops/infra/zoho.tf
@@ -35,3 +35,10 @@ resource "digitalocean_record" "dripfile_txt_zoho_dkim" {
   name     = "zmail._domainkey"
   value    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDC5yvvQmOTga/oreslFFLA8OznLX8XE1hydqJNMy6CrL38lR/fWVZ48GSxIlNS+OCVMRDUb0qEzRL7tXWnJLW58uQPKWgNJZpuUBY8uKVjhdOUHWfGwneRq/q7CAjHAV32otx/0+P5mft7lyUpPkIjbUq2qOBwZReLeDLzo0nFWQIDAQAB"
 }
+
+resource "digitalocean_record" "dripfile_txt_zoho_dmarc" {
+  domain   = digitalocean_domain.dripfile.name
+  type     = "TXT"
+  name     = "_dmarc"
+  value    = "v=DMARC1; p=none; fo=1; rua=mailto:info@dripfile.com"
+}
