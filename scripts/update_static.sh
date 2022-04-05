bacb2e5fcda77c96189d1a52ad9eac6b17c4e1b8
theandrew168 <andrew@shallowbrooksoftware.com>
2022-04-05 14:18:12 -0500

    Update static deps

--- scripts/update_static.sh
+++ scripts/update_static.sh
@@ -2,15 +2,15 @@
 
 # update minireset.css
 # https://jgthms.com/minireset.css/
-curl -L -o internal/static/static/css/minireset.min.css  \
+curl -L -o pkg/static/static/css/minireset.min.css  \
   https://raw.githubusercontent.com/jgthms/minireset.css/master/minireset.min.css
 
 # update Alpine.js
 # https://alpinejs.dev/
-curl -L -o internal/static/static/js/alpine.min.js  \
+curl -L -o pkg/static/static/js/alpine.min.js  \
   https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js
 
 # update htmx
 # https://htmx.org/
-curl -L -o internal/static/static/js/htmx.min.js  \
+curl -L -o pkg/static/static/js/htmx.min.js  \
   https://unpkg.com/htmx.org@1.x.x/dist/htmx.min.js
