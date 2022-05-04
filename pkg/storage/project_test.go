06fbc174d8f8fd40ca954750fd28800fc4fe654d
theandrew168 <andrew@shallowbrooksoftware.com>
2022-05-04 12:50:55 -0500

    Simplify project storage test

--- pkg/storage/project_test.go
+++ pkg/storage/project_test.go
@@ -16,10 +16,11 @@ func mockProject() core.Project {
 	return project
 }
 
-func TestProjectCreate(t *testing.T) {
+func TestProject(t *testing.T) {
 	storage, closer := test.Storage(t)
 	defer closer()
 
+	// create
 	project := mockProject()
 	err := storage.Project.Create(&project)
 	if err != nil {
@@ -29,35 +30,14 @@ func TestProjectCreate(t *testing.T) {
 	if project.ID == "" {
 		t.Fatal("record ID should be non-empty after create")
 	}
-}
 
-func TestProjectCreateUnique(t *testing.T) {
-	storage, closer := test.Storage(t)
-	defer closer()
-
-	project := mockProject()
-	err := storage.Project.Create(&project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// attempt to create the same project again
+	// duplicate
 	err = storage.Project.Create(&project)
 	if !errors.Is(err, core.ErrExist) {
 		t.Fatal("duplicate record should return an error")
 	}
-}
 
-func TestProjectRead(t *testing.T) {
-	storage, closer := test.Storage(t)
-	defer closer()
-
-	project := mockProject()
-	err := storage.Project.Create(&project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	// read
 	got, err := storage.Project.Read(project.ID)
 	if err != nil {
 		t.Fatal(err)
@@ -66,72 +46,8 @@ func TestProjectRead(t *testing.T) {
 	if got.ID != project.ID {
 		t.Fatal("record ID should be match after read")
 	}
-}
 
-func TestProjectUpdate(t *testing.T) {
-	storage, closer := test.Storage(t)
-	defer closer()
-
-	project := mockProject()
-	err := storage.Project.Create(&project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// update fields to new random values
-	project.CustomerID = random.String(8)
-	project.SubscriptionItemID = random.String(8)
-
-	err = storage.Project.Update(project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	got, err := storage.Project.Read(project.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if got.CustomerID != project.CustomerID {
-		t.Fatal("CustomerID should be match after update")
-	}
-	if got.SubscriptionItemID != project.SubscriptionItemID {
-		t.Fatal("SubscriptionItemID should be match after update")
-	}
-}
-
-func TestProjectDelete(t *testing.T) {
-	storage, closer := test.Storage(t)
-	defer closer()
-
-	project := mockProject()
-	err := storage.Project.Create(&project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = storage.Project.Delete(project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// verify that ID isn't present anymore
-	_, err = storage.Project.Read(project.ID)
-	if !errors.Is(err, core.ErrNotExist) {
-		t.Fatal("record ID should be gone after delete")
-	}
-}
-
-func TestProjectReadAll(t *testing.T) {
-	storage, closer := test.Storage(t)
-	defer closer()
-
-	project := mockProject()
-	err := storage.Project.Create(&project)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	// read all
 	projects, err := storage.Project.ReadAll()
 	if err != nil {
 		t.Fatal(err)
@@ -152,5 +68,38 @@ func TestProjectReadAll(t *testing.T) {
 
 	if !found {
 		t.Fatal("record ID should be found in list of all records")
+	}
+
+	// update
+	project.CustomerID = random.String(8)
+	project.SubscriptionItemID = random.String(8)
+
+	err = storage.Project.Update(project)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err = storage.Project.Read(project.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.CustomerID != project.CustomerID {
+		t.Fatal("CustomerID should be match after update")
+	}
+	if got.SubscriptionItemID != project.SubscriptionItemID {
+		t.Fatal("SubscriptionItemID should be match after update")
+	}
+
+	// delete
+	err = storage.Project.Delete(project)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// verify that ID isn't present anymore
+	_, err = storage.Project.Read(project.ID)
+	if !errors.Is(err, core.ErrNotExist) {
+		t.Fatal("record ID should be gone after delete")
 	}
 }
