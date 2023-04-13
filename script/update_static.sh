1b3893fc0653a0884f5e29ebe7fb3b5b406853e7
theandrew168 <andrew@shallowbrooksoftware.com>
2023-04-13 22:30:20 +0100

    Update Alpine.js and htmx

--- script/update_static.sh
+++ script/update_static.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# update tachyons.css
-# https://tachyons.io/
-curl -L -o internal/web/static/css/tachyons.min.css  \
-  https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css
-
 # update Alpine.js
 # https://alpinejs.dev/
-curl -L -o internal/web/static/js/alpine.min.js  \
+curl -L -o static/js/alpine.min.js  \
   https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js
 
 # update htmx
 # https://htmx.org/
-curl -L -o internal/web/static/js/htmx.min.js  \
+curl -L -o static/js/htmx.min.js  \
   https://unpkg.com/htmx.org@1.x.x/dist/htmx.min.js
