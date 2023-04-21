5eec2f3b4209ac7ee11ff20909b5a3a37b962820
theandrew168 <andrew@shallowbrooksoftware.com>
2023-04-21 13:01:16 +0100

    More messing with the CLI

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v2"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/theandrew168/dripfile/internal/config"
 	"github.com/theandrew168/dripfile/internal/database"
+	locationRepo "github.com/theandrew168/dripfile/internal/location/repository"
 	"github.com/theandrew168/dripfile/internal/migrate"
 )
 
@@ -39,6 +41,8 @@ func run() int {
 	}
 	defer pool.Close()
 
+	locationRepo := locationRepo.NewPostgresRepository(pool)
+
 	app := &cli.App{
 		Name:  "dripfile",
 		Usage: "Managed File Transfers as a Service",
@@ -48,6 +52,82 @@ func run() int {
 				Usage: "Applies migrations and exits",
 				Action: func(*cli.Context) error {
 					return migrate.Migrate(logger, pool, migrationFS)
+				},
+			},
+			{
+				Name:  "location",
+				Usage: "Options for managing locations",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "create",
+						Usage: "Creates a new location",
+						Subcommands: []*cli.Command{
+							{
+								Name:  "s3",
+								Usage: "Creates a new S3 location",
+								Flags: []cli.Flag{
+									&cli.StringFlag{
+										Name:     "endpoint",
+										Usage:    "S3 endpoint URL",
+										Required: true,
+									},
+									&cli.StringFlag{
+										Name:     "bucket",
+										Usage:    "S3 bucket name",
+										Required: true,
+									},
+									&cli.StringFlag{
+										Name:     "access_key_id",
+										Usage:    "S3 access key id",
+										Required: true,
+									},
+									&cli.StringFlag{
+										Name:     "secret_access_key",
+										Usage:    "S3 secret access key",
+										Required: true,
+									},
+								},
+								Action: func(*cli.Context) error {
+									location, err := locationRepo.Read("asdf")
+									if err != nil {
+										return err
+									}
+
+									fmt.Printf("%+v\n", location)
+									return nil
+								},
+							},
+						},
+					},
+					{
+						Name:  "read",
+						Usage: "Reads a location by its ID",
+						Action: func(*cli.Context) error {
+							location, err := locationRepo.Read("asdf")
+							if err != nil {
+								return err
+							}
+
+							fmt.Printf("%+v\n", location)
+							return nil
+						},
+					},
+					{
+						Name:  "list",
+						Usage: "Lists all locations",
+						Action: func(*cli.Context) error {
+							locations, err := locationRepo.List()
+							if err != nil {
+								return err
+							}
+
+							for _, location := range locations {
+								fmt.Printf("%+v\n", location)
+							}
+
+							return nil
+						},
+					},
 				},
 			},
 		},
