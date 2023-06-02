0038fc3ddf7483c33b47d9559f86340e41637d96
theandrew168 <andrew@shallowbrooksoftware.com>
2023-06-01 22:54:44 -0500

    Add history domain type

---
+++ internal/history/history.go
@@ -0,0 +1,75 @@
+package history
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidUUID = errors.New("history: invalid UUID")
+)
+
+type History struct {
+	id string
+
+	totalBytes int64
+	startedAt  time.Time
+	finishedAt time.Time
+	transferID string
+}
+
+func New(id string, totalBytes int64, startedAt, finishedAt time.Time, transferID string) (*History, error) {
+	_, err := uuid.Parse(id)
+	if err != nil {
+		return nil, ErrInvalidUUID
+	}
+
+	_, err = uuid.Parse(transferID)
+	if err != nil {
+		return nil, ErrInvalidUUID
+	}
+
+	h := History{
+		id: id,
+
+		totalBytes: totalBytes,
+		startedAt:  startedAt,
+		finishedAt: finishedAt,
+		transferID: transferID,
+	}
+	return &h, nil
+}
+
+func UnmarshalFromStorage(id string, totalBytes int64, startedAt, finishedAt time.Time, transferID string) (*History, error) {
+	h := History{
+		id: id,
+
+		totalBytes: totalBytes,
+		startedAt:  startedAt,
+		finishedAt: finishedAt,
+		transferID: transferID,
+	}
+	return &h, nil
+}
+
+func (h *History) ID() string {
+	return h.id
+}
+
+func (h *History) TotalBytes() int64 {
+	return h.totalBytes
+}
+
+func (h *History) StartedAt() time.Time {
+	return h.startedAt
+}
+
+func (h *History) FinishedAt() time.Time {
+	return h.finishedAt
+}
+
+func (h *History) TransferID() string {
+	return h.transferID
+}
--- migration/0002_create_history_table.sql
+++ migration/0002_create_history_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE history (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    bytes bigint NOT NULL,
+    total_bytes bigint NOT NULL,
     started_at timestamptz NOT NULL,
     finished_at timestamptz NOT NULL,
-    transfer_id uuid NOT NULL,  -- not an FK in case transfer gets deleted
+    transfer_id uuid REFERENCES transfer(id) ON DELETE SET NULL,
 
     -- metadata columns
     created_at timestamptz NOT NULL DEFAULT NOW(),
