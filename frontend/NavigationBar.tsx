eb67bda9a34cce00650fd78dd92bbcd4a95d7d8e
theandrew168 <andrew@shallowbrooksoftware.com>
2023-06-19 21:19:01 -0500

    Create tailwind.config.css file

--- frontend/NavigationBar.tsx
+++ frontend/NavigationBar.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export function NavigationBar() {
 	// TODO: Actually implement a navigation bar
-	return <h1 className="text-3xl font-bold underline">Hello from React! Not really a nav, though!</h1>;
+	return <h1 className="text-4xl font-bold underline">Hello from React! Not really a nav, though!</h1>;
 }
