799cf04ba9c6f3e01838776681adb00def68e572
theandrew168 <andrew@shallowbrooksoftware.com>
2023-09-10 01:40:30 -0500

    Split transfer service into domain and app

--- backend/transfer/service.go
+++ backend/transfer/service.go
@@ -20,22 +20,8 @@ func NewService(locationRepo location.Repository, itineraryRepo itinerary.Reposi
 	return &srvc
 }
 
-func (srvc *Service) Run(itineraryID uuid.UUID) error {
-	i, err := srvc.itineraryRepo.Read(itineraryID)
-	if err != nil {
-		return err
-	}
-
-	from, err := srvc.locationRepo.Read(i.FromLocationID())
-	if err != nil {
-		return err
-	}
-
-	to, err := srvc.locationRepo.Read(i.ToLocationID())
-	if err != nil {
-		return err
-	}
-
+// Perform the transfer given domain objects.
+func (srvc *Service) RunDomain(i *itinerary.Itinerary, from, to *location.Location) error {
 	fromFS, err := from.Connect()
 	if err != nil {
 		return err
@@ -52,4 +38,24 @@ func (srvc *Service) Run(itineraryID uuid.UUID) error {
 	}
 
 	return nil
+}
+
+// Lookup the domain objects from persistent storage and perform the transfer.
+func (srvc *Service) RunApp(itineraryID uuid.UUID) error {
+	i, err := srvc.itineraryRepo.Read(itineraryID)
+	if err != nil {
+		return err
+	}
+
+	from, err := srvc.locationRepo.Read(i.FromLocationID())
+	if err != nil {
+		return err
+	}
+
+	to, err := srvc.locationRepo.Read(i.ToLocationID())
+	if err != nil {
+		return err
+	}
+
+	return srvc.RunDomain(i, from, to)
 }
