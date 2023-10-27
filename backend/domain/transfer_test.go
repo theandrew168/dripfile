3e13c88c12d585883e398beb5ebf18399b61a29f
theandrew168 <andrew@shallowbrooksoftware.com>
2023-10-27 15:06:05 -0500

    Add tests for transfer updates

--- backend/domain/transfer_test.go
+++ backend/domain/transfer_test.go
@@ -14,8 +14,7 @@ func TestNewTransfer(t *testing.T) {
 	to, err := domain.NewMemoryLocation()
 	test.AssertNilError(t, err)
 
-	pattern := "*"
-	itinerary, err := domain.NewItinerary(pattern, from, to)
+	itinerary, err := domain.NewItinerary("*", from, to)
 	test.AssertNilError(t, err)
 
 	transfer, err := domain.NewTransfer(itinerary)
@@ -31,12 +30,49 @@ func TestTransferCanDelete(t *testing.T) {
 	to, err := domain.NewMemoryLocation()
 	test.AssertNilError(t, err)
 
-	pattern := "*"
-	itinerary, err := domain.NewItinerary(pattern, from, to)
+	itinerary, err := domain.NewItinerary("*", from, to)
 	test.AssertNilError(t, err)
 
 	transfer, err := domain.NewTransfer(itinerary)
 	test.AssertNilError(t, err)
 
 	test.AssertNilError(t, transfer.CheckDelete())
+}
+
+func TestTransferUpdateStatus(t *testing.T) {
+	from, err := domain.NewMemoryLocation()
+	test.AssertNilError(t, err)
+
+	to, err := domain.NewMemoryLocation()
+	test.AssertNilError(t, err)
+
+	itinerary, err := domain.NewItinerary("*", from, to)
+	test.AssertNilError(t, err)
+
+	transfer, err := domain.NewTransfer(itinerary)
+	test.AssertNilError(t, err)
+
+	err = transfer.UpdateStatus(domain.TransferStatusComplete)
+	test.AssertNilError(t, err)
+
+	test.AssertEqual(t, transfer.Status(), domain.TransferStatusComplete)
+}
+
+func TestTransferUpdateProgress(t *testing.T) {
+	from, err := domain.NewMemoryLocation()
+	test.AssertNilError(t, err)
+
+	to, err := domain.NewMemoryLocation()
+	test.AssertNilError(t, err)
+
+	itinerary, err := domain.NewItinerary("*", from, to)
+	test.AssertNilError(t, err)
+
+	transfer, err := domain.NewTransfer(itinerary)
+	test.AssertNilError(t, err)
+
+	err = transfer.UpdateProgress(100)
+	test.AssertNilError(t, err)
+
+	test.AssertEqual(t, transfer.Progress(), 100)
 }
