1da23878d244477df82fc1e930c995030e73afec
theandrew168 <andrew@shallowbrooksoftware.com>
2024-02-07 00:20:33 -0600

    Add no-op Vite config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
-import type { Config } from 'tailwindcss';
-import forms from '@tailwindcss/forms';
+import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 export default {
-	content: [
-		"./index.html",
-		"./frontend/**/*.{ts,tsx}",
-	],
-	plugins: [
-		forms,
-	],
+	content: ["./index.html", "./frontend/**/*.{ts,tsx}"],
+	plugins: [forms],
 } satisfies Config;
